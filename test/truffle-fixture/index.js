0d641fbc793f7661848bb895bf448aeb5e56f768
Pham Tu <tubackkhoa@gmail.com>
2022-07-19 10:07:20 +0700

    auto import

--- test/truffle-fixture/index.js
+++ test/truffle-fixture/index.js
@@ -1,17 +1,15 @@
+const path = require('path');
+
 module.exports = async () => {
   const networkId = await web3.eth.net.getId();
   if (networkId === 'test') return;
 
-  await require('./1_initial_migration')();
-  await require('./2_erc1820_registry')();
-  await require('./3_erc1400_token')();
-  await require('./4_erc1400_extension')();
-  await require('./5_erc1400_holdable')();
-  await require('./6_erc1400_certificate_none')();
-  await require('./7_erc1400_certificate_nonce')();
-  await require('./8_erc1400_certificate_salt')();
-  await require('./9_batch_reader')();
-  await require('./10_batch_token_issuer')();
-  await require('./11_delivery_vs_payment')();
-  await require('./12_fund_issuer')();
+  for (const file of require('fs')
+    .readdirSync(__dirname)
+    .filter((file) => file !== 'index.js')
+    .sort(
+      (f1, f2) => parseInt(f1.split('_')[0]) - parseInt(f2.split('_')[0])
+    )) {
+    await require('./' + file)();
+  }
 };
