3884cb096a39d679b3718947c994ed4a55282b64
Pham Tu <tubackkhoa@gmail.com>
2022-08-01 12:17:27 +0700

    add feed document

--- tasks/feeds/ERC1400.ts
+++ tasks/feeds/ERC1400.ts
@@ -1,5 +1,5 @@
 import { ethers, artifacts } from 'hardhat';
-import type { ERC1400 } from 'typechain-types';
+import { ContractHelper } from '../../typechain-types';
 
 const ERC1400 = artifacts.require('ERC1400');
 
@@ -17,23 +17,38 @@ type Args = {
   address: string;
   amount: number;
   holder?: string;
+  name?: string;
+  uri?: string;
 };
 
 export default async function (args: Args) {
   const [owner] = await ethers.getSigners();
+  //@ts-ignore
+  ContractHelper.setSigner(owner);
 
-  const erc1400: ERC1400 = await ERC1400.at(args.address);
+  const erc1400 = ContractHelper.ERC1400.attach(args.address);
 
   console.log('ERC1400 deployed at: ' + erc1400.address);
-  const tokenHolder = args.holder ?? owner.address;
-  await erc1400.issueByPartition(
-    partition1,
-    tokenHolder,
-    args.amount,
-    ZERO_BYTES32,
-    { from: owner.address }
-  );
 
-  const balance = await erc1400.balanceOf(tokenHolder);
-  console.log('balance of', tokenHolder, balance.toString());
+  if (args.name && args.uri) {
+    const documentHash = ethers.utils.id(args.uri);
+    const documentName = ethers.utils.id(args.name);
+    const documentURI = args.uri;
+
+    await erc1400.setDocument(documentName, documentURI, documentHash);
+  }
+
+  if (args.holder) {
+    const tokenHolder = args.holder ?? owner.address;
+    await erc1400.issueByPartition(
+      partition1,
+      tokenHolder,
+      args.amount,
+      ZERO_BYTES32,
+      { from: owner.address }
+    );
+
+    const balance = await erc1400.balanceOf(tokenHolder);
+    console.log('balance of', tokenHolder, balance.toString());
+  }
 }
